456dd9d8e4c74db767a05934e9411d17b9531861
Aaditigupta <42043178+Aaditigupta@users.noreply.github.com>
2020-05-27 14:20:16 +0530

    Update pythonscript.py

--- infra/pythonscript.py
+++ infra/pythonscript.py
@@ -1,9 +1,9 @@
 from python_terraform import *
 
 tf = Terraform(working_dir='/var/lib/jenkins/workspace/Terraform-Python/infra')
-tf.plan()
+tf.plan(out="plan.out", lock = False)
 #approve = {"auto-approve": True}
 print(tf.init(reconfigure=True))
-print(tf.apply(skip_plan = True, lock= False))
-
+#print(tf.apply(skip_plan = True, lock= False))
+print(tf.apply("plan.out", skip_plan= True))
 
