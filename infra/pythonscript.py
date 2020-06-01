8ac0ec9af0b8f965874545e8e8ffa651d0d5137e
Aaditigupta <42043178+Aaditigupta@users.noreply.github.com>
2020-06-01 12:14:48 +0530

    Update pythonscript.py

--- infra/pythonscript.py
+++ infra/pythonscript.py
@@ -1,3 +1,25 @@
+from python_terraform import *
+import argparse
+import time
+ap = argparse.ArgumentParser()
+ap.add_argument("-a", "--arg1", required=True,
+    help="path to argument1")
+ap.add_argument("-b", "--arg2", required=True,
+    help="path to argument2")
+ap.add_argument("-c", "--arg3", required=True,
+    help="path to argument3")
+args = vars(ap.parse_args())
+
+enter = int(args['arg1'])
+instance_id = args['arg2']
+region = args['arg3']
+d = {}
+d[region] = instance_id
+tf = Terraform(working_dir='/var/lib/jenkins/workspace/Terraform-Python/infra', variables={'count':enter, 'aws-region':region , 'AMIs': d })
+tf.plan(no_color=IsFlagged, refresh=False, capture_output=True)
+#approve = {"auto-approve": True}
+print(tf.init(reconfigure=True))
+print(tf.apply(skip_plan = True))
 #from python_terraform import *
 
 #tf = Terraform(working_dir='/var/lib/jenkins/workspace/Terraform-Python/infra')
@@ -7,9 +29,11 @@
 #print(tf.apply(skip_plan = True, lock= False))
 #print(tf.apply("plan.out", skip_plan= True))
 
-from python_terraform import *
+#from python_terraform import *
 
-tf = Terraform(working_dir='/var/lib/jenkins/workspace/Terraform-Python/infra')
-tf.plan()
-print(tf.init(reconfigure=True))
-print(tf.apply(skip_plan = True))
+#tf = Terraform(working_dir='/var/lib/jenkins/workspace/Terraform-Python/infra')
+#tf.plan()
+#print(tf.init(reconfigure=True))
+#print(tf.apply(skip_plan = True))
+
+
